4d9baaeb01ab31356473d2384c9a610807e2a227
NoemiSzabados <noemi@alasdoo.com>
2021-09-18 17:33:40 +0200

    topic information - passing data and styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import TopicInformation from "./views/topicInformation";
 import WordCloud from "./views/wordCloud";
 
 function App() {
+    const topic =
+        {
+        "id": "1751295897__Berlin",
+        "label": "Berlin",
+        "volume": 165,
+        "type": "topic",
+        "sentiment": {
+            "negative": 3,
+            // "neutral": 133,
+            "positive": 29
+        },
+        "sentimentScore": 65,
+        "burst": 13
+        }
+
   return (
     <div className="App">
       <h1>Word Cloud</h1>
       <div className="container">
         <WordCloud />
-        <TopicInformation />
+        {topic && <TopicInformation topic={topic}/>}
       </div>
     </div>
   );
--- src/views/topicInformation/index.js
+++ src/views/topicInformation/index.js
@@ -1,7 +1,19 @@
 import "../../styles/topicInformation.scss";
 
-export default function TopicInformation() {
+export default function TopicInformation({topic}) {
+    const positiveMentions = topic.sentiment.positive ? topic.sentiment.positive : 0;
+    const neutralMentions = topic.sentiment.neutral ? topic.sentiment.neutral : 0;
+    const negativeMentions = topic.sentiment.negative ? topic.sentiment.negative : 0;
+
     return (
-        <div className="infoWrapper">Info</div>
+        <div className="infoWrapper">
+            <div>Information on topic "{topic.label}"</div>
+            <div>Total mentions: {topic.volume}</div>
+            <div>
+                <div>Positive mentions: <span className="positiveMentions">{positiveMentions}</span></div>
+                <div>Neutral mentions: {neutralMentions}</div>
+                <div>Negative mentions: <span className="negativeMentions">{negativeMentions}</span></div>
+            </div>
+        </div>
     );
 }
